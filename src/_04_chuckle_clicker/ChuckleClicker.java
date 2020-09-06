0c94ba63c5f79261a7b92cb777b9f8e20f50cec4
Aditya521623 <54295036+Aditya521623@users.noreply.github.com>
2020-09-05 17:46:22 -0700

    worked on things

--- src/_04_chuckle_clicker/ChuckleClicker.java
+++ src/_04_chuckle_clicker/ChuckleClicker.java
@@ -1,23 +1,40 @@
 package _04_chuckle_clicker;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ChuckleClicker {
+public class ChuckleClicker implements ActionListener {
+	JButton joke= new JButton("joke");
+	JButton punchline= new JButton("punchline");
 	
 	public void makeButtons() {
 JFrame frame= new JFrame();
 JPanel panel= new JPanel();
-JButton joke= new JButton("joke");
-JButton punchline= new JButton("punchline");
 frame.setVisible(true);
 frame.add(panel);
 panel.add(joke);
 panel.add(punchline);
+joke.addActionListener(this);
 frame.pack();
+
 		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	JButton buttonPressed= (JButton) e.getSource();
+		if (buttonPressed==joke) {
+			JOptionPane.showMessageDialog(null, "joke");
+			
+		}
+		else if(buttonPressed==punchline) {
+			JOptionPane.showMessageDialog(null, "punchline");
+		}
 	}
 
 }
